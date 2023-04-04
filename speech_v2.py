66e85f2be96fb50ce15092840a39b6bdb958bb30
Rasmus-Brandt-NTI-Johanneberg <119052518+Rasmus-Brandt-NTI-Johanneberg@users.noreply.github.com>
2023-04-04 09:41:00 +0200

    Dokumentation

--- speech_v2.py
+++ speech_v2.py
@@ -1,3 +1,9 @@
+# Beskrivning:							Spelar in din röst och skickar in den till en OpenAI chatbot,
+#                                       spelar sedan upp svaret från chatbotten med google tts.
+#
+# By:									Rasmus Brandt
+# Date:									2023-04-04
+
 import subprocess  # To kill windows media player
 import speech_recognition as sr # Used to transform the spoken input to a string
 import requests  # To send HTTP requests
@@ -7,20 +13,20 @@ import os  # To play the mp3 file
 
 def audio_in():
     """
-# Input:
-    No input or arguments (only run)
-# Output: 
-    3 Possibles returns:
+    # Input:
+        No input or arguments (only run)
+    # Output: 
+        3 Possibles returns:
 
-    Return 1: recognized_text - Recognized audio in text format
+        Return 1: recognized_text - Recognized audio in text format
 
-    Return 2: None - UnkownValueError
+        Return 2: None - UnknownValueError
 
-    Return 3: None - RequestError
+        Return 3: None - RequestError
 
-# Functionality
-Uses the speech_recognition library to record the users voice.
-Then uses the google tts library to recognize audio and assign value to variable recognized_text
+    # Functionality
+    Uses the speech_recognition library to record the users voice.
+    Then uses the google tts library to recognize audio and assign value to variable recognized_text
     """
 
     r = sr.Recognizer()
@@ -56,13 +62,13 @@ def chatgpt_answer(prompt):
         Recognizes response from ChatBot and assigns answer to variable generated_text
 
     """
-     # Nyckeln för tillgång till ChatGPT
-    api_key = "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"
+     # API Key for OpenAI (insert your own)
+    api_key = "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"
 
-        # The API endpoint for the GPT-2 model
+    # The API endpoint for the GPT-2 model
     url = "https://api.openai.com/v1/engines/text-davinci-002/completions"
 
-        # The headers for the API request
+    # The headers for the API request
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
@@ -74,10 +80,10 @@ def chatgpt_answer(prompt):
         "max_tokens": 512
     }
 
-        # Send the request to the API
+    # Send the request to the API
     response = requests.post(url, headers=headers, json=data)
 
-        # Get the generated text from the API response
+    # Get the generated text from the API response
     generated_text = response.json()["choices"][0]["text"]
 
     print("Generated text: " + generated_text)
@@ -126,17 +132,19 @@ def is_wmp_running():
         else:
             return False
 
-    # Here is the main loop of the program which ends if you again == n.
+    # Here is the main loop of the program which ends if the user inputs "n". (again == n)
 while True:
     if is_wmp_running():
         subprocess.call("taskkill /IM wmplayer.exe /F", shell=True)
+
+    print("Please wait for the listening prompt.")
 
     recognized_text = audio_in()
     if recognized_text:
         generated_text = chatgpt_answer(recognized_text)
         text_to_speech(generated_text)
 
-    again = input("Do you want to continue? (y/n)")
+    again = input("Do you want to continue chatting? (y/n)")
     if again == 'n':
         subprocess.call("taskkill /IM wmplayer.exe /F", shell=True)
         break
